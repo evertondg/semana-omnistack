c840e211b72c8be8ddd1d5699f32c9da50a1269f
Everton De Grande <evertondg@hotmail.com.br>
2019-08-08 17:42:54 -0300

    Adicionado rotas para Cadastrar dev, Like e Dislike. Adicionado Conexao com banco de dados e CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 const routes = require("./routes");
 
 const server = express();
 
+
+mongoose.connect('mongodb+srv://evertondg:010114@everton-estudo-s9xxz.mongodb.net/banco?retryWrites=true&w=majority', {
+  useNewUrlParser: true
+});
+
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
