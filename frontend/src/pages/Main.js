3a8f8692cd6a1832ce7bf914c48d28a486467786
Everton De Grande <evertondg@hotmail.com.br>
2019-08-12 23:52:55 -0300

    Adicionado socket.io - Funções de Match no frontend React

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,4 +1,5 @@
 import React,{useEffect,useState} from 'react'
+import io from 'socket.io-client';
 import {Link} from 'react-router-dom'
 import './Main.css'
 
@@ -9,10 +10,12 @@ import api from '../services/api'
 import logo from '../assets/logo.svg'
 import like from '../assets/like.svg'
 import dislike from '../assets/dislike.svg'
+import itsamatch from '../assets/itsamatch.png'
 
 export default function Main({ match }){
 
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(null);
 
     useEffect(()=>{
 
@@ -28,6 +31,18 @@ export default function Main({ match }){
 
     }, [match.params.id])
 
+
+    useEffect(()=>{
+        const socket = io('http://localhost:3333', 
+            { 
+                query: {user: match.params.id}
+            });
+
+        socket.on('match', dev =>{
+            setMatchDev(dev);
+        });
+
+    }, [match.params.id]);
 
     async function handleLike(id){
         await api.post(`/devs/${id}/likes`,null,{
@@ -76,6 +91,16 @@ export default function Main({ match }){
                 </ul>
             ) : (
                 <div className="empty">Acabou :(</div>
+            )}
+
+            {matchDev &&(
+                <div className="match-container">
+                    <img  src={itsamatch} alt="It's a Match"/>
+                    <img className="avatar" src={matchDev.avatar} alt=""/>
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+                    <button type="button" onClick={()=> setMatchDev(null)}>FECHAR</button>
+                </div>
             )}
       </div>  
     )
